9aaa2dd9a1924285fe1281d515e56b7bae4842ff
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-27 20:46:23 +0530

    search ui

--- twitter/lib/constants/ui_constants.dart
+++ twitter/lib/constants/ui_constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:twitter/constants/constants.dart';
+import 'package:twitter/features/explore/view/explore_view_dart.dart';
 import 'package:twitter/features/tweet/widgets/tweet_list.dart';
 import 'package:twitter/theme/pallete.dart';
 
@@ -18,7 +19,7 @@ class UIConstants {
 
   static const List<Widget> bottomTabBarPages = [
     TweetList(),
-    Text('Search Screen'),
+    ExploreView(),
     Text('Noti')
   ];
 }
---
+++ twitter/lib/features/explore/view/explore_view_dart.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter/theme/pallete.dart';
+
+class ExploreView extends ConsumerStatefulWidget {
+  const ExploreView({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ExploreViewState();
+}
+
+class _ExploreViewState extends ConsumerState<ExploreView> {
+  final searchControlller = TextEditingController();
+
+  @override
+  void dispose() {
+    searchControlller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appBarTextFieldBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(50),
+      borderSide: const BorderSide(
+        color: Pallete.searchBarColor,
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: SizedBox(
+          height: 50,
+          child: TextField(
+            controller: searchControlller,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(10).copyWith(
+                left: 20,
+              ),
+              fillColor: Pallete.searchBarColor,
+              filled: true,
+              enabledBorder: appBarTextFieldBorder,
+              focusedBorder: appBarTextFieldBorder,
+              hintText: 'Search Twitter',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
